99be58eac7218b0e5b87731de6a5eba90bb07921
Chris Hayen <chris@bayesianhealth.com>
2020-11-09 14:25:15 -0500

    Removed workflow

--- entrypoint_test.sh
+++ entrypoint_test.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -l
 
+if [ "-z" ${GITHUB_SHA} ]; then
+  export GITHUB_SHA=7dd2b57bb0ff93fd830a7c6346b5d35257594389
+fi
 
-export GITHUB_SHA=7dd2b57bb0ff93fd830a7c6346b5d35257594389
-export GITHUB_REF=refs/tags/0.10
+if [ "-z" ${GITHUB_REF} ]; then
+  export GITHUB_REF=refs/tags/0.10
+fi
+
 
 # Test Release
 export GITHUB_EVENT_NAME=release
@@ -18,7 +23,7 @@ export GITHUB_EVENT_NAME=push
 
 out=$(./entrypoint.sh)
 
-if [ "$out" != "::set-output name=tag::7dd2b57bb0ff93fd830a7c6346b5d35257594389" ]; then
-  echo "Non-Release\nexpected ::set-output name=tag::7dd2b57bb0ff93fd830a7c6346b5d35257594389 \ngot $out"
+if [ "$out" != "::set-output name=tag::$GITHUB_SHA" ]; then
+  echo "Non-Release\nexpected ::set-output name=tag::$GITHUB_SHA \ngot $out"
   exit 1
 fi
