95da364bf5ce83bb4e28ec55f161aef7121d4ac2
Chris Hayen <chris@bayesianhealth.com>
2020-11-09 14:07:21 -0500

    Updated test to include vars

--- entrypoint_test.sh
+++ entrypoint_test.sh
@@ -1,10 +1,16 @@
 #!/bin/sh -l
 
+if [ "-z" ${GITHUB_SHA} ]; then
+  export GITHUB_SHA=7dd2b57bb0ff93fd830a7c6346b5d35257594389
+fi
+
+if [ "-z" ${GITHUB_REF} ]; then
+  export GITHUB_REF=refs/tags/0.10
+fi
+
+
 # Test Release
 export GITHUB_EVENT_NAME=release
-export GITHUB_SHA=7dd2b57bb0ff93fd830a7c6346b5d35257594389
-export GITHUB_REF=refs/tags/0.10
-
 out=$(./entrypoint.sh)
 
 if [ "$out" != "::set-output name=tag::0.10" ]; then
@@ -14,8 +20,6 @@ fi
 
 # Test Non-Release
 export GITHUB_EVENT_NAME=push
-export GITHUB_SHA=7dd2b57bb0ff93fd830a7c6346b5d35257594389
-export GITHUB_REF=refs/tags/0.10
 
 out=$(./entrypoint.sh)
 
